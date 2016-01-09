32abd71546f269d327db597fc9d3010df38e4de3
admin <admin@example.com>
2016-01-09 14:23:19 +0500

    B4 last video lesson

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+
 
 # Create your views here.
+#def index(request):
+#	return HttpResponse("Hello world from my blog")
+
 def index(request):
-	return HttpResponse("Hello world from my blog")
+	data = {'mydata': "Hello world from my blog !"}
+	return render(request, 'blog/index.html', data)
